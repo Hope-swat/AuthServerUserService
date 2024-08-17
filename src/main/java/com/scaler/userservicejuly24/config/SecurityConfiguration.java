f7a30a3830de1afbe75cc69764494e8416580799
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-08-17 16:08:41 +0530

    UserService

--- src/main/java/com/scaler/userservicejuly24/config/SecurityConfiguration.java
+++ src/main/java/com/scaler/userservicejuly24/config/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.scaler.userservicejuly24.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
